614d69f634347930a313e3930389572490d747d9
a.kondratiev <a.kondratiev@ddemo.ru>
2019-02-18 13:23:00 +0300

    #29931_db

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=mfb7917n_yii2',
+    'username' => 'mfb7917n_yii2',
+    'password' => 'm&CVqDt4',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
